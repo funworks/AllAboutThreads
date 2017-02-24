4509f5ac07b53eb90270776c25fb554e88d4af2a
funworks <harsh.sahajwani@gmail.com>
2017-02-25 01:44:19 +0530

    Create Thread using Anonymous class

---
+++ src/com/threads/anonymous/App.java
@@ -0,0 +1,27 @@
+package com.threads.anonymous;
+
+/**
+ * This app creates worker threads using Anonymous class
+ */
+public class App {
+
+	public static void main(String[] args) {
+		Thread thread1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for (int i=1; i<=10; i++) {
+					System.out.println(Thread.currentThread().getName() + " - " + i);
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread1.setName("Thread 1");
+		thread1.start();
+	}
+
+}
